141fc6456be23854303ba7c79d06c71bba062a77
Chandrag <mikhail.b.smirnov@gmail.com>
2026-06-09 19:13:56 +0300

    7 - 1

---
+++ Applied_information_technologies/6LR/7LR/app2/app.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+PORT = os.getenv('PORT', 8082)
+PONG = os.getenv('PONG', 'pong-from-app-2')
+
+# Счётчик запросов
+counter = 0
+
+@app.route('/')
+def index():
+    global counter
+    counter += 1
+    return f'''
+    <!DOCTYPE html>
+    <html>
+    <head><meta charset="UTF-8"><title>{PONG}</title></head>
+    <body style="font-family: Arial; text-align: center; padding: 50px;">
+        <h1>{PONG}</h1>
+        <p>Счётчик запросов: {counter}</p>
+        <p>Порт: {PORT}</p>
+        <hr>
+        <a href="/ping">/ping</a> | <a href="/info">/info</a>
+    </body>
+    </html>
+    '''
+
+@app.route('/ping')
+def ping():
+    return f'{PONG}\n'
+
+@app.route('/info')
+def info():
+    return f'App: {PONG} | Port: {PORT} | Requests: {counter}\n'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(PORT))
